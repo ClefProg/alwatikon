a77e76f12db3b052c5873b9c18e3c6f11a50ce0f
hshehabu <hamzashehabu457@gmail.com>
2026-06-16 17:37:12 +0300

    Remove unused pricing-related models and their access rights from the Alwatikon pricing module.

--- alwatikon_pricing/models/__init__.py
+++ alwatikon_pricing/models/__init__.py
@@ -4,6 +4,3 @@
 from . import res_config_settings
 from . import product_display_name
 from . import product_product
-from . import pricing_record
-from . import usd_cost_log
-from . import profit_split
